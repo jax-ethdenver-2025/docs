865c0d70bdd89a0b6247db95e54cdf0e59276d24
al <alex@getquotient.ai>
2025-03-28 16:07:17 -0400

    fix: update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,61 +1,60 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jax-ethdenver-2025.github.io",
-	base: "docs",
-	integrations: [
-		starlight({
-			title: 'jax docs.',
-			logo: { src: './src/assets/icon.svg' },
-			social: {
-				github: 'https://github.com/jax-ethdenver-2025',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					slug: 'docs/intro'
-				},
-				{
-					label: 'Storage',
-					items: [
-						{
-							slug: 'docs/storage/iroh'
-						},
-						{
-							slug: 'docs/storage/blake3'
-						},
-					]
-				},
-				{
-					label: 'Consensus',
-					items: [
-						{
-							slug: 'docs/consensus/peer_trust'
-						},
-						{
-							slug: 'docs/consensus/eigen_layer'
-						},
-					]
-				}
-			],
-			customCss: ['./src/tailwind.css'],
-			locales: {
-				root: {
-					label: 'English',
-					lang: 'en',
-				},
-			},
-		}),
-		tailwind({ applyBaseStyles: false }),
-		icon({
-			include: {
-				lucide: ['database', 'github', 'book'],
-			},
-		}),
-	],
+  site: "https://jax-ethdenver-2025.github.io",
+  integrations: [
+    starlight({
+      title: "jax docs.",
+      logo: { src: "./src/assets/icon.svg" },
+      social: {
+        github: "https://github.com/jax-ethdenver-2025",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          slug: "docs/intro",
+        },
+        {
+          label: "Storage",
+          items: [
+            {
+              slug: "docs/storage/iroh",
+            },
+            {
+              slug: "docs/storage/blake3",
+            },
+          ],
+        },
+        {
+          label: "Consensus",
+          items: [
+            {
+              slug: "docs/consensus/peer_trust",
+            },
+            {
+              slug: "docs/consensus/eigen_layer",
+            },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
+        },
+      },
+    }),
+    tailwind({ applyBaseStyles: false }),
+    icon({
+      include: {
+        lucide: ["database", "github", "book"],
+      },
+    }),
+  ],
 });
